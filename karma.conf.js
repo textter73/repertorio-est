b2b6b754cd56ab626ca28523ed309201fc2c0be0
Angel Ortiz <jortiz@fmf.mx>
2025-07-27 04:18:42 -0600

    cambio de nombre del proyecto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/repertorio-est'),
+      dir: require('path').join(__dirname, './coverage/control-estonantzin'),
       subdir: '.',
       reporters: [
         { type: 'html' },
